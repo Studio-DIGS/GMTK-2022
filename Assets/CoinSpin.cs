ab88c431fb4bb15ae94f6edaa8669618bfbe4797
jucobee <jucobee@gmail.com>
2022-07-16 20:57:37 -0700

    coin spin

---
+++ Assets/CoinSpin.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpin : MonoBehaviour
+{
+    public CoinPhysics _physics;
+    private Rigidbody _rb;
+    public float modifier;
+    public float stopSpeed;
+
+    private float spinSpeed;
+    private float xEnd;
+
+    void Awake()
+    {
+        _rb = _physics.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spinSpeed = modifier * _rb.velocity.z;
+        transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+
+        if (_rb.velocity.magnitude < 1f)
+        {
+            xEnd += stopSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-90,-90,0), xEnd);
+        }
+    }
+}
