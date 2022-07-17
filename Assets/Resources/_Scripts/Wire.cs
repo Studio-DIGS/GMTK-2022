8221c1b84acad3c1bab33695886a581e851b4314
Jordan Wong <jordanhwong1@gmail.com>
2022-07-16 20:19:16 -0700

    Updated Wire To Allow For Cut/Uncut

--- Assets/Resources/_Scripts/Wire.cs
+++ Assets/Resources/_Scripts/Wire.cs
@@ -6,6 +6,8 @@ public class Wire : MonoBehaviour
 {
     //Wire Vars
     [SerializeField] AudioSource wireCutSFX;
+    [SerializeField] GameObject uncutWire;
+    [SerializeField] GameObject cutWire;
     private LevelManager levelManager;
     private Material hitWireMaterial;
 
@@ -27,7 +29,8 @@ public class Wire : MonoBehaviour
         if (other.gameObject.tag == "Coin" && !hasBeenHit)
         {
             GameObject.FindWithTag("Player").GetComponent<PlayerStateManager>().score.AddPoints(value);
-            this.GetComponent<MeshRenderer>().material = hitWireMaterial;
+            uncutWire.SetActive(false);
+            cutWire.SetActive(true);
 
             hasBeenHit = true;
             wireCutSFX.Play();
