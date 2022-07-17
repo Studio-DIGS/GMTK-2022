96ac1744666a3f6f3a7c0eb6068f5083712a7155
jucobee <jucobee@gmail.com>
2022-07-17 04:29:17 -0700

    slots stop on icon

--- Assets/Resources/_Scripts/SlotSpin.cs
+++ Assets/Resources/_Scripts/SlotSpin.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class SlotSpin : MonoBehaviour
 {
+    public int icon;
     public int id;
-    public float spinSpeed = -1000f;
+    public float spinSpeed;
+    private float maxSpinSpeed = -1000f;
+    private float acceleration = 0.2f;
+    private bool spinning = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +19,19 @@ public class SlotSpin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+        if (spinning)
+        {
+            spinSpeed = Mathf.Lerp(spinSpeed, maxSpinSpeed, acceleration * Time.deltaTime);
+            transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+        }
     }
 
     private void StopSpin(int id)
     {
         if (id == this.id)
         {
-            
+            spinning = false;
+            transform.eulerAngles = new Vector3((-90 - 60*icon), -90, 270);
         }
     }
 
